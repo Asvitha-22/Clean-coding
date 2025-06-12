88fe63953e23e93e9f44ea6695277fb7cb9269fe
Asvitha-22 <asvishree@gmail.com>
2025-06-12 15:39:56 +0530

    Update Follows_law_of_demeter.cpp
    updated rules for LOD

--- Follows_law_of_demeter.cpp
+++ Follows_law_of_demeter.cpp
@@ -43,3 +43,11 @@ int main() {
     string city = order.getShippingCity();  // ✅ Clean, decoupled
     cout << city << endl;
 }
+
+/* Rules
+
+    A member function is allowed to call other member functions in its own class scope directly.
+    A member function is allowed to call member functions on member variables that are in its class scope directly.
+    If a member function has parameters, the member function is allowed to call the member functions of these parameters directly.
+    If a member function creates local objects, the member function is allowed to call member functions on those local objects. Object form of Law of Demeter (DemeterW3; Martin, 2003)
+*/
